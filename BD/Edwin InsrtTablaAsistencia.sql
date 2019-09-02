743d864905377425d435d63905dc2ad10465517d
LogroEdwin <edwinlogro1997@gmail.com>
2019-09-01 22:58:31 -0400

    cambios en la tablas

---
+++ BD/Edwin InsrtTablaAsistencia.sql
@@ -0,0 +1,52 @@
+-- los obreros supervisores almaceneros y prensistas trabajan de 6:00 a 14:00 pm
+-- Obreros
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,1,'2016-02-01 6:00:00','2016-02-01 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,2,'2016-02-02 6:00:00','2016-02-02 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,3,'2016-02-03 6:00:00','2016-02-03 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,4,'2016-03-04 6:00:00','2016-03-04 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,5,'2016-02-05 14:00:00','2016-02-05 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,6,'2016-02-06 8:00:00','2016-02-06 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,7,'2016-02-07 8:00:00','2016-02-07 20:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,8,'2016-03-08 6:00:00','2016-03-08 14:00:00',8);
+-- Spervisores
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,9,'2016-03-01 6:00:00','2016-03-01 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,10,'2016-03-02 8:00:00','2016-03-02 12:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,11,'2016-03-03 14:00:00','2016-03-04 16:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,12,'2016-03-04 6:00:00','2016-03-04 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,13,'2016-03-05 6:00:00','2016-03-05 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,14,'2016-03-06 6:00:00','2016-03-06 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,15,'2016-03-07 6:00:00','2016-03-07 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,16,'2016-03-08 6:00:00','2016-03-08 14:00:00',8);
+-- Almacenero
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,17,'2016-03-11 6:00:00','2016-03-11 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,18,'2016-03-12 6:00:00','2016-03-12 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,19,'2016-03-13 6:00:00','2016-03-14 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,20,'2016-03-14 6:00:00','2016-03-14 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,21,'2016-03-15 6:00:00','2016-03-15 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,22,'2016-03-16 6:00:00','2016-03-16 14:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,23,'2016-03-17 14:00:00','2016-03-17 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,24,'2016-03-18 8:00:00','2016-03-17 12:00:00',8);
+-- guardias de seguridad
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,25,'2016-04-01 8:00:00','2016-04-01 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,26,'2016-04-02 8:00:00','2016-04-02 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,27,'2016-04-03 8:00:00','2016-04-03 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,28,'2016-04-04 8:00:00','2016-04-04 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,29,'2016-04-05 8:00:00','2016-04-05 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,30,'2016-04-06 8:00:00','2016-04-06 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,31,'2016-04-07 8:00:00','2016-04-07 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,32,'2016-04-08 8:00:00','2016-04-08 20:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,33,'2016-05-01 20:00:00','2016-05-01 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,34,'2016-05-02 20:00:00','2016-05-02 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,33,'2016-05-03 20:00:00','2016-05-04 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,36,'2016-05-04 20:00:00','2016-05-04 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,37,'2016-05-05 20:00:00','2016-05-05 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,38,'2016-05-06 20:00:00','2016-05-06 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,39,'2016-05-07 20:00:00','2016-05-07 08:00:00',12);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,40,'2016-05-08 20:00:00','2016-05-08 08:00:00',12);
+-- Secretaria
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,41,'2016-03-11 6:14:00','2016-03-11 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,42,'2016-03-12 6:00:00','2016-03-12 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,43,'2016-03-13 14:00:00','2016-03-14 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,44,'2016-03-14 14:00:00','2016-03-14 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,45,'2016-03-14 14:00:00','2016-03-15 22:00:00',8);
+INSERT INTO Asistencia (idAsistencia,idEmpleado,horaFechaEntrada, horaFechaSalida,totalHoras) VALUES (null,46,'2016-03-16 6:00:00','2016-03-16 14:00:00',8);
--- BD/EdwinInsertTablaEmpleado.sql
+++ BD/EdwinInsertTablaEmpleado.sql
@@ -1,10 +1,10 @@
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,7,1,'9283','Julian','Roberto','Tigazi','Caizaluisa','1995-01-13','24569803','M',null,'19783458','3469642','jTigazi9283','9283',true);
+VALUES(null,4,1,'9283','Julian','Roberto','Tigazi','Caizaluisa','1995-01-13','24569803','M',null,'19783458','3469642','jTigazi9283','9283',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,8,1,'8374','Carlos','Robert','Torres','Caceres','1993-03-12','2387872','M',null,'57623919','2263832','cTorres8374','8374',true);
+VALUES(null,7,1,'8374','Carlos','Robert','Torres','Caceres','1993-03-12','2387872','M',null,'57623919','2263832','cTorres8374','8374',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
@@ -16,7 +16,7 @@ VALUES(null,3,1,'0864','Jhosselin','Deysi','Lopez','Arce','1995-01-17','14569807
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,4,2,'1357','Abigail','Romelia','Arcenas','Ariene','1992-03-02','26238772','F',null,'45679012','2263832','aArcenas1357','1357',true);
+VALUES(null,3,2,'1357','Abigail','Romelia','Arcenas','Ariene','1992-03-02','26238772','F',null,'45679012','2263832','aArcenas1357','1357',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
@@ -29,7 +29,7 @@ VALUES(null,6,2,'9753','Patty','Anai','Robles','Caceres','1992-06-19','44569803'
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,7,3,'3579','Michael','Camilo','Joria','Arce','1992-03-02','142387872','M',null,'47623923','0663832','mJoria3579','3579',true);
+VALUES(null,9,3,'3579','Michael','Camilo','Joria','Arce','1992-03-02','142387872','M',null,'47623923','0663832','mJoria3579','3579',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
@@ -49,15 +49,15 @@ VALUES(null,6,1,'7685','Aron','Jonas','Loaiza','Caceres','1991-08-20','30796211'
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,7,1,'7594','Gorge','Andy','Torrico','Cardenas','1993-01-13','14569803','M',null,'09783458','2469642','gTorrico7594','7594',true);
+VALUES(null,9,1,'7594','Gorge','Andy','Torrico','Cardenas','1993-01-13','14569803','M',null,'09783458','2469642','gTorrico7594','7594',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,8,1,'2873','Elvis','josue','Antenas','Perez','1993-03-12','2382872','M',null,'57623919','2263832','eAntenas2873','2873',true);
+VALUES(null,3,1,'2873','Elvis','josue','Antenas','Perez','1993-03-12','2382872','M',null,'57623919','2263832','eAntenas2873','2873',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,8,1,'7685','Yolanda','Margarita','Flores','Caiza','1996-02-22','60796211','F',null,'3224344','1223234','yFlores7685','7685',true);
+VALUES(null,3,1,'7685','Yolanda','Margarita','Flores','Caiza','1996-02-22','60796211','F',null,'3224344','1223234','yFlores7685','7685',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
@@ -65,14 +65,40 @@ VALUES(null,3,1,'0196','Rosmery','Carmen','Lopez','Arce','1995-01-17','14569807'
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,4,2,'7685','Jael','Romelia','Perez','Naria','1991-03-02','26238772','F',null,'45679012','2263832','jPerez7685','Perez',true);
+VALUES(null,3,2,'7685','Jael','Romelia','Perez','Naria','1991-03-02','26238772','F',null,'45679012','2263832','jPerez7685','7685',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
-VALUES(null,5,2,'0099','Mary','Esther','Tarqui','Taca','1992-01-23','10796211','F',null,'14234445','2232347','mTarqui0099','0099',true);
+VALUES(null,9,2,'0099','Mary','Esther','Tarqui','Taca','1992-01-23','10796211','F',null,'14234445','2232347','mTarqui0099','0099',true);
 
 INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
 genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
 VALUES(null,6,2,'0088','Patty','Anai','Flores','Mamani','1991-06-19','84569803','F',null,'49783458','7469642','aFlores0088','0088',true);
+
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,9,2,'5674','Rafael','Ronaldo','Correa','Delgado','1985-01-03','00238772','M',null,'02619012','2263832','rCorrea5674','5674',true);
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,9,3,'8392','Aron','Carlos','Tarqui','Torres','1993-01-13','00796211','M',null,'04234445','0032347','aTarqui8392','8392',true);
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,9,1,'2910','Josias','Alfredo','Caiza','Caiza','1998-11-09','014569803','M',null,'09783458','0469642','jCaiza2910','2910',true);
+
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,10,2,'1111','Robert','Ramiro','Cachi','Delgado','1982-01-03','10238772','M',null,'01619012','02263832','rCachi1111','1111',true);
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,3,3,'2222','Jose','Carlos','Mamani','Torres','1997-01-13','22296210','M',null,'01234445','0232347','jMamani2222','2222',true);
+
+INSERT INTO Empleado(idEmpleado,idCargo,idEstadoCivil,CI,primerNombre,segundoNombre,apellidoPaterno,apellidoMaterno,fechaNacimiento,codeRFID,
+genero,fotografia,numeroCelular,numeroFijo,usuario,Contrasenia,activo) 
+VALUES(null,3,1,'3333','Josias','Alfredo','Cardenas','Caiza','1997-12-19','04569031','M',null,'09783458','0469642','jCardenas3333','3333',true);
 
 
